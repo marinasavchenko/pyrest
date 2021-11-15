e3a5a702e540b4933f25e506e69e6c7dbac4194e
marynasavchenko <msavchenko.abacuspro@gmail.com>
2021-11-15 20:05:26 +0100

    implemented post, get methods

--- restapp.py
+++ restapp.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 restapp = Flask(__name__)
 
@@ -14,16 +14,26 @@ dealers = [
     }
 ]
 
+
 # POST dealer/{dealerName}
 @restapp.route('/dealer', methods=['POST'])
 def create_dealer():
-    pass
+    request_data = request.get_json()
+    dealer = {
+        'name': request_data['name'],
+        'cars': []
+    }
+    dealers.append(dealer)
+    return jsonify(dealer)
 
 
 # GET dealer/{dealerName}
 @restapp.route('/dealer/<string:name>', methods=['GET'])
 def get_dealer(name):
-    pass
+    for dealer in dealers:
+        if dealer['name'] == name:
+            return jsonify(dealer)
+        return jsonify({'message': 'dealer is not found'})
 
 
 # GET dealer/
@@ -34,14 +44,26 @@ def get_dealers():
 
 # POST dealer/{dealerName}/{car}
 @restapp.route('/dealer/<string:name>/car', methods=['POST'])
-def create_car():
-    pass
+def create_car(name):
+    request_data = request.get_json()
+    for dealer in dealers:
+        if dealer['name'] == name:
+            car = {
+                'name': request_data['name'],
+                'price': request_data['price']
+            }
+            dealer['cars'].append(car)
+            return jsonify(car)
+        return jsonify({'message': 'dealer is not found'})
 
 
 # GET dealer/{dealerName}/{car}
 @restapp.route('/dealer/<string:name>/car', methods=['GET'])
-def get_car():
-    pass
+def get_car(name):
+    for dealer in dealers:
+        if dealer['name'] == name:
+            return jsonify({'cars': dealer['cars']})
+        return jsonify({'message': 'cars are not found'})
 
 
 restapp.run(port=5000)
